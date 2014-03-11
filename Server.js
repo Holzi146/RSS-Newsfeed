d489fc11ebcfcd4ec7a9a7dd7b62540f612ab882
Christoph Holzhauser <holzhauser.christoph@gmx.at>
2014-03-11 12:48:43 +0100

    11.03.2014: Update 1

--- Server.js
+++ Server.js
@@ -8,10 +8,36 @@ http.createServer(function (req, res) {
     body += chunk.toString('utf8');
   });
   req.on('end', function() {
-    console.log(body);
-    res.end();
+    console.log("Requested URL: " + body);
+    GetRSS(body);
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end();
   });
 }).listen(8888);
 
-console.log("Service is running...");
+console.log("Service is running...\n");
+
+function GetRSS(url)  {
+    
+  var FeedParser = require(__dirname)
+  , fs = require('fs')
+  , feed = __dirname+'/news.xml';
+
+  fs.createReadStream(feed)
+    .on('error', function (error) {
+      console.error(error);
+    })
+    .pipe(new FeedParser())
+    .on('error', function (error) {
+      console.error(error);
+    })
+    .on('meta', function (meta) {
+      console.log('===== %s =====', meta.title);
+    })
+    .on('readable', function() {
+      var stream = this, item;
+      while (item = stream.read()) {
+        console.log('Got article: %s', item.title || item.description);
+      }
+    });  
+}
